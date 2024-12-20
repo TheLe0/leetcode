25a4a03ebad906e8746673b2e455d925b5cfc06a
Leonardo B. Tosin <leob.tosin@hotmail.com>
2024-12-20 19:23:56 -0300

    feat: Two Sum algorithm

--- src/leetcode/lib.go
+++ src/leetcode/lib.go
@@ -1,29 +1,48 @@
 package main
 
 import (
-    "strconv"
+	"strconv"
 )
+
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/description/
+func twoSum(nums []int, target int) []int {
+	numsMap := make(map[int]int)
+
+	for idx, value := range nums {
+
+		rest := target - value
+
+		if index, exists := numsMap[rest]; exists {
+			return []int{index, idx}
+		}
+
+		numsMap[value] = idx
+	}
+
+	return nil
+}
 
 // 738. Monotone Increasing Digits
 // https://leetcode.com/problems/monotone-increasing-digits/description/
 
 func validateDigits(n int) bool {
-    numStr := strconv.Itoa(n)
+	numStr := strconv.Itoa(n)
 
-    for i := 0; i < len(numStr)-1; i++ {
-        currentDigit := numStr[i]
-        nextDigit := numStr[i+1]
+	for i := 0; i < len(numStr)-1; i++ {
+		currentDigit := numStr[i]
+		nextDigit := numStr[i+1]
 
-        if currentDigit > nextDigit {
-            return false
-        }
-    }
+		if currentDigit > nextDigit {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
 
 func monotoneIncreasingDigits(n int) int {
-    goNext := true
+	goNext := true
 	monotoneValue := n
 
 	for goNext {
--- src/leetcode/lib_test.go
+++ src/leetcode/lib_test.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-    "testing"
+	"reflect"
+	"testing"
 )
 
 func TestMonotoneIncreasingDigits(t *testing.T) {
-    tests := []struct {
-        input    int
-        expected int
-    }{
-        {10, 9},
-        {1234, 1234},
-        {332, 299},
-    }
+	tests := []struct {
+		input    int
+		expected int
+	}{
+		{10, 9},
+		{1234, 1234},
+		{332, 299},
+	}
 
-    for _, test := range tests {
-        result := monotoneIncreasingDigits(test.input)
-        if result != test.expected {
-            t.Errorf("monotoneIncreasingDigits(%d) = %d; want %d", test.input, result, test.expected)
-        }
-    }
+	for _, test := range tests {
+		result := monotoneIncreasingDigits(test.input)
+		if result != test.expected {
+			t.Errorf("monotoneIncreasingDigits(%d) = %d; want %d", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		nums     []int
+		target   int
+		expected []int
+	}{
+		{nums: []int{2, 7, 11, 15}, target: 9, expected: []int{0, 1}},
+		{nums: []int{3, 2, 4}, target: 6, expected: []int{1, 2}},
+		{nums: []int{3, 3}, target: 6, expected: []int{0, 1}},
+	}
+
+	for _, test := range tests {
+		result := twoSum(test.nums, test.target)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("For nums=%v and target=%d, expected %v, but got %v",
+				test.nums, test.target, test.expected, result)
+		}
+	}
 }
